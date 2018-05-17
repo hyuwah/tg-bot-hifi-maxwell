7c17e51ff94e2271e3b7a9415c594599eb82d1e7
Muhammad Wahyudin <muhammad.whydn@gmail.com>
2018-05-18 00:04:01 +0700

    test newChatMembers

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const TeleBot = require('telebot');
 const bot = new TeleBot(process.env.apikey)
 
-bot.on('text', (msg) => msg.reply.text(msg.text));
 
-bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome!'));
+bot.on('newChatMembers', (msg) => msg.reply.text("Selamat datang "+msg.new_chat_members[0].first_name + " " + msg.new_chat_members[0].last_name));
+
+
+bot.on(['/start', '/hello'], (msg) => msg.reply.text('Welcome! '+ msg.from.username ));
 
 bot.start();
